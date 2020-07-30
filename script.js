77bd0ee8235416a8b6aa1d1c23de90a1f5bfc7f3
Rose Ralak <ralakrose@gmail.com>
2020-07-30 23:02:28 +0300

    edit script.js

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
+/*Calculator input */
 function display(num){
    document.getElementById("numinput").value = document.getElementById("numinput").value+num;
 }
+/*Result after performing operation */
 function equals(){
     let ans = document.getElementById("numinput").value
     if(ans){
     document.getElementById("numinput").value=eval(ans);
     }
 }
+/*Clear all content on the input area */
 function clear(){
     document.getElementById("numinput").value ="";
 }
+/*Clear last entry*/
 function backspace(){
     let disp = document.getElementById("numinput").value;
     document.getElementById("numinput").value=disp.substring(0,disp.length-1);
